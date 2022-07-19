aba00591fd9c2f5e5c2a23ace80c136527f4a775
Max Tsogt <bazarsadtsogtjargal@gmail.com>
2022-07-19 16:40:05 +0900

    feat: add tag service

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
   @Get()
   findAll() {
-    return ['dream', 'code', 'programming'];
+    return this.tagService.findAll();
   }
 }
