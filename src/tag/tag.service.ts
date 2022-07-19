5f3d5321813a02094424980f562e4a06207aeb51
Max Tsogt <bazarsadtsogtjargal@gmail.com>
2022-07-19 16:46:53 +0900

    feat: implement absolute pathand watch changes with nodemon

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
   findAll(): string[] {
-    return ['dream', 'code', 'programming', 'nestjs'];
+    return ['dream', 'code', 'programming'];
   }
 }
